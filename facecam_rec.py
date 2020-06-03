462dbfd44e7190c468297dba20340d784ad7f893
NanoClem <clemdecoop.pro@gmail.com>
2020-06-03 02:31:02 +0200

    using Pillow to draw a bow around found faces

--- facecam_rec.py
+++ facecam_rec.py
@@ -1,4 +1,5 @@
-from PIL import Image
+from PIL import Image, ImageDraw
+import numpy as np
 import face_recognition
 import os
 
@@ -8,8 +9,8 @@ def getImgFiles(foldPath: str, extensions: tuple) -> list:
     
     Parameters
     -----
-        foldPath (str) -- path of the folder where your 
-        extensions (list) -- [description]
+        foldPath (str) -- path of the folder where files are
+        extensions (tuple) -- file extensions filter
 
     Raises
     -----
@@ -17,7 +18,7 @@ def getImgFiles(foldPath: str, extensions: tuple) -> list:
     
     Returns
     -----
-        list -- A list of image filenames
+        list -- A list of filenames
     """
     if os.path.isdir(foldPath):
         ret = [f for f in os.listdir(foldPath) if os.path.isfile(os.path.join(foldPath, f)) and f.endswith(extensions)]
@@ -29,13 +30,30 @@ def getImgFiles(foldPath: str, extensions: tuple) -> list:
 
 dirpath  = os.getcwd()
 datasets = os.path.join(dirpath, 'datasets')
-imgs = getImgFiles(datasets, ('.jpg', '.jpeg', '.png'))
+imgs     = getImgFiles(datasets, ('.jpg', '.jpeg', '.png'))
+resizer  = lambda x, y, s: (int(x*s), int(y*s))              # resize any image to s of its size
 
-image = face_recognition.load_image_file(os.path.join(datasets, imgs[0]))
-face_locations = face_recognition.face_locations(image)
+# load image with pillow
+image = Image.open(os.path.join(datasets, imgs[0]))
+# image = image.resize(resizer(*image.size, 0.25), Image.ANTIALIAS).convert('RGB')   # resize img to make face recognition faster
+data = np.array(image)
 
+# load img with face_recognition
+# image = face_recognition.load_image_file(os.path.join(datasets, imgs[0]))
+ 
+face_locations = face_recognition.face_locations(data)
+img_cpy = image.copy()
+
+# Draw a box around each faces
+draw = ImageDraw.Draw(img_cpy)
 for face in face_locations:
     top, right, bottom, left = face
-    face_img = image[top:bottom, left:right]
-    output = Image.fromarray(face_img)
-    output.show()
+    draw.rectangle( ((left, top), (right, bottom)), outline=(255, 0, 0), width=1 )
+    # Create a new image for each face
+    # face_img = image[top:bottom, left:right]
+    # output = Image.fromarray(face_img)
+    # output.show()
+
+del draw
+img_cpy = img_cpy.resize(resizer(*img_cpy.size, 2), Image.ANTIALIAS)
+img_cpy.show()
