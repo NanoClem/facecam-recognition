a649aebb40e071900afa6fd1c66384b27c2781b3
NanoClem <clemdecoop.pro@gmail.com>
2020-06-17 19:45:52 +0200

    add configs for flask_bcrypt

--- app/configs.py
+++ app/configs.py
@@ -8,10 +8,16 @@ load_dotenv('.env')
 
 class Config(object):
     
+    # MONGO
     MONGO_DBNAME = os.environ.get('MONGO_DBNAME')
     MONGO_URI    = os.environ.get('MONGO_URI')
 
+    # KEYS
     SECRET_KEY = os.environ.get('SECRET_KEY')
     WTF_CSRF_SECRET_KEY = os.environ.get('WTF_CSRF_SECRET_KEY')
+
+    # FLASK BCRYPT
+    BCRYPT_LOG_ROUNDS = 12
+    BCRYPT_HASH_PREFIX = '2b'
 
     DEBUG = True
