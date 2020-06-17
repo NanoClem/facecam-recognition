bf5060527396e0fc160f1916bb28fe57e9a84e43
NanoClem <clemdecoop.pro@gmail.com>
2020-06-17 19:49:05 +0200

    implementing user login and logout routes

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,24 +1,49 @@
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, flash, request
+from flask_login import login_required, login_user, logout_user, current_user
 
-from ..auth import login_blueprint as lbp
-from ..auth import register_blueprint as rbp
-from .forms import LoginForm
-from .controllers import login_user
+from ..auth import login_blueprint as linbp, logout_blueprint as loutbp, register_blueprint as rbp
+from .forms import LoginForm, RegisterForm
+from .controllers import LoginController
+from .models import User
 
 
-
-@lbp.route('/', methods=['GET', 'POST'])
+# LOGIN
+@linbp.route('/', methods=['GET', 'POST'])
 def login():
     """Login page view of the app
     """
+    # Check if user is already logged in
+    if current_user.is_authenticated:
+        return redirect(url_for('dashboard.dashboard'))
+    
+    # Init login form
     form = LoginForm()
-    if login_user(form):
-        return redirect(url_for('home.home'))
+    if request.method == 'POST' and form.validate_on_submit():
+        username = form.username.data
+        if LoginController.try_login(username, form.password.data):
+            user = User(username=username)
+            login_user(user, remember=form.remember_me.data)
+            return redirect(url_for('dashboard.dashboard'))
+        else:
+            flash('Login Unsuccessful. Wrong email or password', 'danger')
+    
     return render_template('login.html', title='Login', form=form)
 
 
+# LOGOUT
+@loutbp.route('/', methods=['GET'])
+@login_required
+def logout():
+    """Logout user and redirection to login
+    """
+    logout_user()
+    return redirect(url_for('login.login'))
+
+
+# REGISTER
 @rbp.route('/', methods=['GET', 'POST'])
 def register():
     """Register page view of the app
     """
-    return render_template('register.html', title='Register')
+    form = RegisterForm()
+    return render_template('register.html', title='Register', form=form)
