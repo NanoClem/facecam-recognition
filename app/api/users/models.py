7f4b9b5ad5004fc8104ff12fca2f1a658a5cb5ac
NanoClem <clemdecoop.pro@gmail.com>
2020-06-17 19:47:25 +0200

    add new database fields for users

--- app/api/users/models.py
+++ app/api/users/models.py
@@ -8,8 +8,11 @@ def create_user_model(ns):
     return ns.model('User', {
         "_id"        : fields.String(description='unique identifier of the frame'),
         "pseudo"     : fields.String(descritpion='pseudo of the user'),
-        'avatar'     : fields.Integer(description='avatard img of the user'),
+        'avatar'     : fields.String(description='avatard img of the user', default=''),
         'email'      : fields.String(description='email of the user'),
         'password'   : fields.String(description='password of the user'),
+        'api_key'    : fields.String(description='api key of the user', default=None),
+        'banned'     : fields.Boolean(description='if the user is banned from any service', default=False),
+        'admin'      : fields.Boolean(description='if the user has admin rights or not', default=False),
         'created_at' : fields.String(description='date of creation')
     })
