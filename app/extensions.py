7be60b951b6d7cdaf13091eb10ef7bb34d8bf1ce
NanoClem <clemdecoop.pro@gmail.com>
2020-06-17 19:46:07 +0200

    new flask_bcrypt extension

--- app/extensions.py
+++ app/extensions.py
@@ -1,8 +1,10 @@
 from flask_pymongo import PyMongo
 from flask_wtf import CSRFProtect
 from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
 
 
 mongo         = PyMongo()
 csrf          = CSRFProtect()
 login_manager = LoginManager()
+flask_bcrypt  = Bcrypt()
