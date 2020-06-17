0f0f657706f4c76034730caa635bcb6ed8d39c84
NanoClem <clemdecoop.pro@gmail.com>
2020-06-17 19:48:03 +0200

    add login_required protection

--- app/dashboard/routes.py
+++ app/dashboard/routes.py
@@ -1,9 +1,11 @@
 from flask import render_template
+from flask_login import login_required
 
 from ..dashboard import dashboard_blueprint as bp
 
 
 @bp.route('/')
+@login_required
 def dashboard():
     """Dashboard page view of the app
     """
