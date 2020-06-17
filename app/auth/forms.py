f740db74e4b6ae6f8bc3a8b23854c4d3c0576d35
NanoClem <clemdecoop.pro@gmail.com>
2020-06-17 19:45:20 +0200

    create forms

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,12 +1,34 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, SubmitField, BooleanField
+from wtforms import PasswordField, SubmitField, BooleanField, StringField
 from wtforms.fields.html5 import EmailField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, EqualTo, Length
 
 
-
+# LOGIN
 class LoginForm(FlaskForm):
-    username    = EmailField('Email', validators=[DataRequired()])
-    password    = PasswordField('Password', validators=[DataRequired()])
+
+    username = EmailField('Email', validators=[
+        DataRequired(message='email required'),
+        Length(min=2, max=35), 
+        Email(message='please enter a valid email')])
+
+    password    = PasswordField('Password', validators=[DataRequired(message='password required')])
     remember_me = BooleanField('remember me')
     submit      = SubmitField('Log in')
+
+
+# REGISTER
+class RegisterForm(FlaskForm):
+
+    username = EmailField('Email', validators=[
+        DataRequired(message='email required'),
+        Length(min=2, max=35), 
+        Email(message='please enter a valid email')])
+
+    password = PasswordField('Password', validators=[
+        DataRequired(message='password required'),
+        EqualTo('confirm', message='passwords must match')])
+
+    confirm = PasswordField('Confirm password')
+    pseudo  = StringField('Pseudo', validators=[DataRequired(message='pseudo required')])
+    submit  = SubmitField('Create account')
