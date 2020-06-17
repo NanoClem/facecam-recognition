2d2581e98bb0d3bf3bb3ea80bb0506fbd14e918d
NanoClem <clemdecoop.pro@gmail.com>
2020-06-17 19:48:46 +0200

    implement flask_bcrypt functions

--- app/auth/controllers.py
+++ app/auth/controllers.py
@@ -1,77 +1,32 @@
 from flask import flash, redirect
-import bcrypt
+
+from ..extensions import flask_bcrypt
+from .models import User
 
 
 
-def hash_password(password: str, salt: bytes=bcrypt.gensalt(), key_bytes:int=32, rounds: int=12) -> str:
-    """[summary]
-    
-    Parameters
-    -----
-        password (str) -- [description]
-        salt (str) -- [description]
-        key_bytes (int) -- [description] (default: 32)
-        rounds (int) -- [description] (default: 12)
-    
-    Returns
-    -----
-        str -- [description]
-    """
-    hash = bcrypt.kdf(
-        password=str.encode(password),
-        salt=salt,
-        desired_key_bytes=key_bytes,
-        rounds=rounds
-    )
-    return hash
+class LoginController(object):
+
+    @classmethod
+    def check_password(cls, plain_psswd: str, hash_psswd: bytes) -> bool:
+        """
+        """
+        return flask_bcrypt.check_password_hash(hash_psswd, plain_psswd)
+
+    @classmethod
+    def try_login(cls, username: str, password: str) -> bool:
+        """
+        """
+        user = User.getByEmail(username)
+        if user:
+            return cls.check_password(password, user['password'])
+        return False
 
 
+class RegisterController(object):
 
-def check_password(hash_psswd: str) -> bool:
-    """Check for matching hash in database of the given password
-    
-    Parameters
-    -----
-        hash_psswd (str) -- hash version of a user's password input
-    
-    Returns
-    -----
-        bool -- hashed password matched in database or not
-    """
-    ## TODO: request api to check user password
-    db_psswd = ""
-    return True #bcrypt.checkpw(db_psswd, hash_psswd)
-
-
-
-def check_credentials(username: str, hash_psswd: str) -> bool:
-    """Check user credentials in database
-    
-    Returns
-    -----
-        bool -- [description]
-    """             
-    return check_password(hash_psswd)    ##TODO: check email in db
-
-
-
-def login_user(form) -> bool:
-    """Log a user by checking form validation and user credentials
-    
-    Parameters
-    -----
-        form (flask_wtf.FlaskForm) -- form class with user credentials
-
-    Returns
-    -----
-        bool -- User successfuly loged in or not
-    """
-    if form.validate_on_submit():
-        username    = form.username.data
-        password    = hash_password(form.password.data)
-        remember_me = form.remember_me.data
-        # temp log message
-        flash('Login requested for user {}, remember_me={}'.format(username, remember_me))
-        return check_credentials(username, password)
-    
-    return False
+    @classmethod
+    def hash_password(cls, password: str) -> bytes:
+        """
+        """
+        return flask_bcrypt.generate_password_hash(password).decode('utf-8')
