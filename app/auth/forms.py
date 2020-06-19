28b534448368c23c5181991a0564215ad2fde41e
NanoClem <clemdecoop.pro@gmail.com>
2020-06-19 02:03:40 +0200

    adding custom email and pseudo validators

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,7 +1,20 @@
 from flask_wtf import FlaskForm
 from wtforms import PasswordField, SubmitField, BooleanField, StringField
 from wtforms.fields.html5 import EmailField
-from wtforms.validators import InputRequired, DataRequired, Email, EqualTo, Length
+from wtforms.validators import InputRequired, DataRequired, Email, EqualTo, Length, ValidationError
+
+from .models import User
+
+
+# CUSTOM VALIDATORS
+def email_exists(form, field):
+    if User.getByEmail(field.data):
+        raise ValidationError('email already exists')
+
+def pseudo_exists(form, field):
+    if User.getByPseudo(field.data):
+        raise ValidationError('pseudo already exists')
+
 
 
 # LOGIN
@@ -22,12 +35,16 @@ class RegisterForm(FlaskForm):
     username = EmailField('Email', validators=[
         InputRequired(message='email required'),
         Length(min=2, max=35), 
-        Email(message='please enter a valid email')])
+        Email(message='please enter a valid email'),
+        email_exists])
 
     password = PasswordField('Password', validators=[
         InputRequired(message='password required'),
         EqualTo('confirm', message='passwords must match')])
 
+    pseudo  = StringField('Pseudo', validators=[
+        InputRequired(message='pseudo required'),
+        pseudo_exists])
+
     confirm = PasswordField('Confirm password')
-    pseudo  = StringField('Pseudo', validators=[InputRequired(message='pseudo required')])
     submit  = SubmitField('Create account')
