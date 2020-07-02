4a99465931e6ce3394b85e71c313b52a5ce24b0c
NanoClem <clemdecoop.pro@gmail.com>
2020-07-02 05:27:18 +0200

    renaming arg name in parser

--- app/api/parsers.py
+++ app/api/parsers.py
@@ -6,7 +6,7 @@ from werkzeug.datastructures import FileStorage
 my_parser = reqparse.RequestParser()
 
 # Add parsing arguments
-my_parser.add_argument('file',
+my_parser.add_argument('img',
                         location='files',
                         type=FileStorage,
                         required=True)
